c849fd424e207f379832e0fe35ba268da4da8e72
thelanranger <37283974+thelanranger@users.noreply.github.com>
2025-12-02 15:38:33 -0500

    Notifications bugfix

--- DisableChromeNotifications.bat
+++ DisableChromeNotifications.bat
@@ -9,7 +9,7 @@ xcopy "%~dp0*.*" C:\PCR\ /e /i /y /exclude:"%~dp0exclude.txt"
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
 echo Disable Chrome Notifications........ >> C:\PCR\UpdateLog.txt
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
-Powershell -ExecutionPolicy "bypass" -NoProfile -Command "C:\PCR\RemoteApps\DisableChromeNotifications.ps1" >> C:\PCR\UpdateLog.txt
+Powershell -ExecutionPolicy "bypass" -NoProfile -Command "C:\PCR\DisableChromeNotifications.ps1" >> C:\PCR\UpdateLog.txt
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
 echo End Disable Chrome Notifications, %date%, %time% >> C:\PCR\UpdateLog.txt
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
--- DisableEdgeNotifications.bat
+++ DisableEdgeNotifications.bat
@@ -9,7 +9,7 @@ rem xcopy "%~dp0*.*" C:\PCR\ /e /i /y /exclude:"%~dp0exclude.txt"
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
 echo Disable Edge Notifications........ >> C:\PCR\UpdateLog.txt
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
-Powershell -ExecutionPolicy "bypass" -NoProfile -Command "C:\PCR\RemoteApps\DisableEdgeNotifications.ps1" >> C:\PCR\UpdateLog.txt
+Powershell -ExecutionPolicy "bypass" -NoProfile -Command "C:\PCR\DisableEdgeNotifications.ps1" >> C:\PCR\UpdateLog.txt
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
 echo End Disable Edge Notifications, %date%, %time% >> C:\PCR\UpdateLog.txt
 echo ----------------------------------------------------------------------------------------------------- >> C:\PCR\UpdateLog.txt
